2f5fb0674d139ed300345742631e7eec40cea78b
Billy K. Poon <bkpoon@lbl.gov>
2023-11-15 11:34:39 -0800

    Try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,15 @@
 
 #PYTHON=python2.7
 
-export CPPFLAGS=-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=1 ${CPPFLAGS}
-export CFLAGS=-stdlib=libc++ ${CFLAGS}
-export CXXFLAGS=-stdlib=libc++ ${CXXFLAGS}
-export LDLAGS=-stdlib=libc++ ${LDFLAGS}
+# export CPPFLAGS=-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=1 ${CPPFLAGS}
+# export CFLAGS=-stdlib=libc++ ${CFLAGS}
+# export CXXFLAGS=-stdlib=libc++ ${CXXFLAGS}
+# export LDLAGS=-stdlib=libc++ ${LDFLAGS}
+
+export CPPFLAGS=-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=1
+export CFLAGS=-stdlib=libc++ -mmacosx-version-min=10.12
+export CXXFLAGS=-stdlib=libc++ -mmacosx-version-min=10.12
+export LDLAGS=-stdlib=libc++ -mmacosx-version-min=10.12
 
 cd wxPython
 $PYTHON build-wxpython.py --prefix=$PREFIX --build_dir=../bld --osx_cocoa --install
